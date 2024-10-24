52bd239b2d69fe2e8ec042672dd93aa9e25edef9
Dhikshith Reddy <dhikshithreddy12@gmail.com>
2024-10-24 17:47:10 +0530

    telecom manager

--- android/src/main/kotlin/co/stackfinance/call_watcher/call_watcher/CallWatcherPlugin.kt
+++ android/src/main/kotlin/co/stackfinance/call_watcher/call_watcher/CallWatcherPlugin.kt
@@ -12,6 +12,8 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.telecom.TelecomManager
+
 
 /** CallWatcherPlugin */
 class CallWatcherPlugin: FlutterPlugin, MethodCallHandler {
